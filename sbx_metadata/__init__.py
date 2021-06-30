d991b46ad3074ec3d0770e54557a98c44a214c72
Anne Schumacher <anne.schumacher@gu.se>
2021-06-30 18:07:14 +0200

    fix some default values

--- sbx_metadata/__init__.py
+++ sbx_metadata/__init__.py
@@ -17,14 +17,17 @@ __config__ = [
     Config("sbx_metadata.korp", default=True, description="Whether the corpus will be published in Korp"),
     Config("sbx_metadata.downloads", default=[], description="Downloadable files belonging to the corpus"),
     Config("sbx_metadata.interface", default=[], description="List of interfaces where the corpus is available"),
-    Config("sbx_metadata.contact_info", default="sbx-default", description="Object containing information about the contact person"
+    Config("sbx_metadata.contact_info", default="sbx-default",
+           description="Object containing information about the contact person"
            "for the resource"),
     Config("sbx_metadata.trainingdata", default=False, description="Whether the corpus is intended as training data"),
-    Config("sbx_metadata.metashare_host", "bark", description="Remote host to copy META-SHARE export to."),
-    Config("sbx_metadata.metashare_path", "/home/fksparv/sb-metadata/meta-share/corpus",
+    Config("sbx_metadata.metashare_host", "fksparv@bark.spraakdata.gu.se",
+           description="Remote host to copy META-SHARE export to."),
+    Config("sbx_metadata.metashare_path", "/home/fksparv/metadata/meta-share/corpus",
            description="Path on remote host to copy META-SHARE export to."),
-    Config("sbx_metadata.json_export_host", "bark", description="Remote host to copy JSON metadata export to."),
-    Config("sbx_metadata.json_export_path", "/home/fksparv/sb-metadata/json/corpus",
+    Config("sbx_metadata.json_export_host", "fksparv@bark.spraakdata.gu.se",
+           description="Remote host to copy JSON metadata export to."),
+    Config("sbx_metadata.json_export_path", "/home/fksparv/metadata/json/corpus",
            description="Path on remote host to copy JSON metadata export to.")
 ]
 
