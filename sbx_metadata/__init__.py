bdc0ae56eac621dc4e94fbb11faa8db5a7d93479
Anne Schumacher <anne.schumacher@gu.se>
2021-07-07 15:25:21 +0200

    set stats_export True by default

--- sbx_metadata/__init__.py
+++ sbx_metadata/__init__.py
@@ -13,7 +13,7 @@ __config__ = [
     #        "one, two or more languages (monolingual, bilingual or multilingual)"),
     Config("sbx_metadata.xml_export", default=False, description="Whether XML export may be published."
            "Values: scrambled, original, false"),
-    Config("sbx_metadata.stats_export", default=False, description="Whether token frequency export may be published"),
+    Config("sbx_metadata.stats_export", default=True, description="Whether token frequency export may be published"),
     Config("sbx_metadata.korp", default=True, description="Whether the corpus will be published in Korp"),
     Config("sbx_metadata.downloads", default=[], description="Downloadable files belonging to the corpus"),
     Config("sbx_metadata.interface", default=[], description="List of interfaces where the corpus is available"),
